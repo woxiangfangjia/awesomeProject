aeedecd5ef31861877d5a6e4cf295a867dcb68ff
聪聪 <129638040+woxiangfangjia@users.noreply.github.com>
2023-12-29 19:53:03 +0800

    Update filesystem.go

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -3,6 +3,7 @@ package fileserver
 import (
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -11,21 +12,29 @@ import (
 const uploadDirectory = "./uploads"
 
 // HandleFileUpload 处理文件上传
-func HandleFileUpload(w http.ResponseWriter, r *http.Request) {
+func HandleFileUpload(w http.ResponseWriter, r *http.Request) error {
 	// 限制上传文件的大小
-	r.ParseMultipartForm(10 << 20) // 10 MB
+	//err := r.ParseMultipartForm(10 << 20)
+	//if err != nil {
+	//	return
+	//} // 10 MB
 
 	file, handler, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, "Error retrieving the file", http.StatusBadRequest)
-		return
+		return err
 	}
-	defer file.Close()
+	defer func(file multipart.File) {
+		err := file.Close()
+		if err != nil {
+			return
+		}
+	}(file)
 
 	// 创建上传目录
 	if err := os.MkdirAll(uploadDirectory, os.ModePerm); err != nil {
 		http.Error(w, "Error creating the upload directory", http.StatusInternalServerError)
-		return
+		return err
 	}
 
 	// 生成上传文件的路径
@@ -33,18 +42,27 @@ func HandleFileUpload(w http.ResponseWriter, r *http.Request) {
 	dst, err := os.Create(filePath)
 	if err != nil {
 		http.Error(w, "Error creating the file on the server", http.StatusInternalServerError)
-		return
+		return err
 	}
-	defer dst.Close()
+	defer func(dst *os.File) {
+		err := dst.Close()
+		if err != nil {
+			return
+		}
+	}(dst)
 
 	// 将上传的文件内容拷贝到服务器上的文件中
 	if _, err := io.Copy(dst, file); err != nil {
 		http.Error(w, "Error copying file to server", http.StatusInternalServerError)
-		return
+		return err
 	}
 
 	// 返回上传成功的消息
-	fmt.Fprintf(w, "File %s uploaded successfully", handler.Filename)
+	_, err = fmt.Fprintf(w, "File %s uploaded successfully", handler.Filename)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // HandleFileDownload 处理文件下载
